09a340f03888a295bc3bf8a925fc91b5f3a5b161
Daniel Müller <deso@posteo.net>
2026-05-21 14:27:15 -0700

    Test

--- libbpf-rs/tests/bin/src/stream.bpf.c
+++ libbpf-rs/tests/bin/src/stream.bpf.c
@@ -5,15 +5,22 @@
 #include <bpf/bpf_helpers.h>
 
 /*
+ * Declare the kfunc here because the pinned vmlinux.h revision predates
+ * kernel commit d806f3101276 ("bpf: Migrate bpf_stream_vprintk() to
+ * KF_IMPLICIT_ARGS"), which renamed bpf_stream_vprintk_impl back to
+ * bpf_stream_vprintk and dropped the aux argument.
+ */
+extern int bpf_stream_vprintk(int stream_id, const char *fmt__str,
+                              const void *args, __u32 len__sz) __weak __ksym;
+
+/*
  * Trigger writing of some messages to stdout & stderr streams.
  */
 SEC("syscall")
 int trigger_streams(void *ctx)
 {
-    unsigned long long args[1] = {};
-
-    bpf_stream_vprintk_impl(1, "stdout", args, 0, NULL);
-    bpf_stream_vprintk_impl(2, "stderr", args, 0, NULL);
+    bpf_stream_printk(1, "stdout");
+    bpf_stream_printk(2, "stderr");
     return 0;
 }
 
